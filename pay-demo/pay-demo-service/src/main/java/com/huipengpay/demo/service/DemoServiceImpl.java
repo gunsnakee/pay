589ef0495fbff982c1280d6f43bc7fc737282406
gunsnakee <gunsnakee@foxmail.com>
2014-11-13 21:17:51 +0800

    demo

---
+++ pay-demo/pay-demo-service/src/main/java/com/huipengpay/demo/service/DemoServiceImpl.java
@@ -0,0 +1,31 @@
+package com.huipengpay.demo.service;
+
+import com.huipengpay.demo.api.DemoService;
+import com.huipengpay.demo.api.dto.DemoDto;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2014/11/13.
+ */
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public int inert(DemoDto demoDto) {
+        return 0;
+    }
+
+    @Override
+    public int update(DemoDto demoDto) {
+        return 0;
+    }
+
+    @Override
+    public DemoDto findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<DemoDto> list() {
+        return null;
+    }
+}
