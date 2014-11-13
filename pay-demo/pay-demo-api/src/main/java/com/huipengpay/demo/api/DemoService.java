f3a9701cacc3df3a358833c5c71049ea37e5cf2c
gunsnakee <gunsnakee@foxmail.com>
2014-11-13 22:58:49 +0800

    demo

--- pay-demo/pay-demo-api/src/main/java/com/huipengpay/demo/api/DemoService.java
+++ pay-demo/pay-demo-api/src/main/java/com/huipengpay/demo/api/DemoService.java
@@ -3,12 +3,25 @@ package com.huipengpay.demo.api;
 import com.huipengpay.demo.api.dto.DemoDto;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2014/11/13.
  */
 public interface DemoService  {
-     int inert(DemoDto demoDto);
+
+     int transInt(int i);
+     byte transByte(byte b);
+     boolean transBool(boolean b);
+     String transStr(String s);
+
+     Map<String,DemoDto> transMap(Map<String,DemoDto> map);
+     Set<DemoDto> transSet(Set<DemoDto> set);
+     List<DemoDto> transList(List<DemoDto> list);
+     DemoDto[] transArray(DemoDto[] array);
+
+     int insert(DemoDto demoDto);
      int update(DemoDto demoDto);
      DemoDto findById(int id);
      List<DemoDto> list();
--- pay-demo/pay-demo-service/src/main/java/com/huipengpay/demo/service/DemoServiceImpl.java
+++ pay-demo/pay-demo-service/src/main/java/com/huipengpay/demo/service/DemoServiceImpl.java
@@ -4,13 +4,56 @@ import com.huipengpay.demo.api.DemoService;
 import com.huipengpay.demo.api.dto.DemoDto;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2014/11/13.
  */
 public class DemoServiceImpl implements DemoService {
+
     @Override
-    public int inert(DemoDto demoDto) {
+    public int transInt(int i) {
+        return 0;
+    }
+
+    @Override
+    public byte transByte(byte b) {
+        return 0;
+    }
+
+    @Override
+    public boolean transBool(boolean b) {
+        return false;
+    }
+
+    @Override
+    public String transStr(String s) {
+        return null;
+    }
+
+    @Override
+    public Map<String, DemoDto> transMap(Map<String, DemoDto> map) {
+        return null;
+    }
+
+    @Override
+    public Set<DemoDto> transSet(Set<DemoDto> set) {
+        return null;
+    }
+
+    @Override
+    public List<DemoDto> transList(List<DemoDto> list) {
+        return null;
+    }
+
+    @Override
+    public DemoDto[] transArray(DemoDto[] array) {
+        return new DemoDto[0];
+    }
+
+    @Override
+    public int insert(DemoDto demoDto) {
         return 0;
     }
 
@@ -29,3 +72,4 @@ public class DemoServiceImpl implements DemoService {
         return null;
     }
 }
+
